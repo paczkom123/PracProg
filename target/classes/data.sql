03975750447fa04b7eaf0096b9d9c422ff121ff1
paczkom123 <45208358+paczkom123@users.noreply.github.com>
2021-03-26 17:35:53 +0100

    result introduction

---
+++ target/classes/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO QUESTION VALUES(1, 'y', true, 'Priorytetem polityki kulturalnej państwa powinno być wzmacnianie tożsamości narodowej.');
+INSERT INTO QUESTION VALUES(2, 'y', false, 'Aborcja powinna być legalna w szerszym katalogu przypadków niż obecnie.');
+INSERT INTO QUESTION VALUES(3, 'y', false, 'Należy ułatwić dostęp do broni palnej.');
+INSERT INTO QUESTION VALUES(4, 'x', false, 'Hodowla zwierząt na futra powinna pozostać dozwolona.');
+INSERT INTO QUESTION VALUES(5, 'x', false, 'Należy podwyższyć podatki dla najlepiej zarabiających osób.');
+INSERT INTO QUESTION VALUES(6, 'y', true, 'Pensja minimalna powinna zostać znacznie podwyższona.');
+INSERT INTO QUESTION VALUES(7, 'y', true, 'Priorytetem polityki kulturalnej państwa powinno być wzmacnianie tożsamości narodowej.');
+INSERT INTO QUESTION VALUES(8, 'y', true, 'Priorytetem polityki kulturalnej państwa powinno być wzmacnianie tożsamości narodowej.');
+INSERT INTO QUESTION VALUES(9, 'y', true, 'Priorytetem polityki kulturalnej państwa powinno być wzmacnianie tożsamości narodowej.');
+INSERT INTO QUESTION VALUES(10, 'y', true, 'Priorytetem polityki kulturalnej państwa powinno być wzmacnianie tożsamości narodowej.');
